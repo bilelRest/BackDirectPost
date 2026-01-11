2aad68d351402584f48c46e535bbb92d2b99765c
bilelRest <bilel.apirest.tech>
2026-01-11 01:05:09 +0100

    apres back front

--- src/main/java/tn/poste/myship/sec/config/WebSecurityConfig.java
+++ src/main/java/tn/poste/myship/sec/config/WebSecurityConfig.java
@@ -33,7 +33,7 @@ public class WebSecurityConfig {
                 .csrf(AbstractHttpConfigurer::disable)
                 .sessionManagement(session -> session.sessionCreationPolicy(SessionCreationPolicy.STATELESS))
                 .authorizeHttpRequests(auth -> auth
-                        .requestMatchers("/api/auth/**","/auth/**").permitAll() // Endpoint pour login/register
+                        .requestMatchers("/auth/**", "/api/auth/**", "**/auth/**").permitAll() // Endpoint pour login/register
                         .anyRequest().authenticated()
                 );
 
